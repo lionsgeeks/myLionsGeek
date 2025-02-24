473179890f1841756a6aad26f8510f3b605f863f
unknown <nassomanassima37@gmail.com>
2025-02-24 12:06:46 +0000

    add landing page

---
+++ app/Livewire/Places/Place.php
@@ -0,0 +1,17 @@
+<?php
+
+// namespace App\Livewire\Places;
+
+// use Livewire\Component;
+
+// class Place extends Component
+// {
+//     public function render()
+//     {
+//         return view('livewire.places.place', [
+//             'places' => Place::all(),
+//             'showModal' => false
+            
+//         ]);
+//     }
+// }
