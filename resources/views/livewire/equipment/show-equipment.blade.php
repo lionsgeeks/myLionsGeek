4758ba9328a0ea623d41ba0b7f4fe26cea205816
walid-cadi <cadiwalid02@gmail.com>
2025-02-25 15:16:02 +0000

    equipment ui7

--- resources/views/livewire/equipment/show-equipment.blade.php
+++ resources/views/livewire/equipment/show-equipment.blade.php
@@ -1,105 +1,107 @@
-<div>
-    <button class="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition"
+<div class="bg-[#101010] min-h-screen text-white flex flex-col p-8 gap-y-5">
+    <div class="flex items-center justify-between ">
+        <h1 class="text-3xl">Equipment</h1>
+        <button class="px-4 py-2 bg-yellow-500 text-white rounded-md  transition"
         x-on:click.prevent="$dispatch('open-modal', 'modal' )">{{ __('create equipment') }}</button>
-    <x-modal name="modal" :show="$errors->isNotEmpty()">
-        <div class="p-5 gap-2 flex flex-col items-center">
-            <form wire:submit='create' class="flex flex-col items-center">
-                <label class="flex flex-col gap-y-2">
-                    <span>reference :</span>
-                    <input type="text" wire:model="reference">
-                    @error('reference')
-                        <em class="text-red-500">{{ $message }}</em>
-                    @enderror
-                </label>
+        <x-modal name="modal" :show="$errors->isNotEmpty()">
+            <div class="p-5 gap-2 flex flex-col items-center bg-[#111827] ">
+                <form wire:submit='create' class="flex flex-col gap-y-3  w-full">
+                    <h1 class="text-yellow-500 text-xl font-bold border-b pb-2">Create Equipment</h1>
+                    <label class="w-full">
+                        <span class=" pb-2 ">reference :</span>
+                        <input class="w-full border border-gray-600 rounded-lg p-2 bg-gray-800 text-white focus:ring-2 focus:border-none focus:ring-[#fee814] focus:outline-none" type="text" wire:model="reference">
+                        @error('reference')
+                            <em class="text-red-500">{{ $message }}</em>
+                        @enderror
+                    </label>
 
-                <label class="flex flex-col gap-y-2">
-                    <span>mark :</span>
-                    <input type="text" wire:model="mark">
-                    @error('mark')
-                        <em class="text-red-500">{{ $message }}</em>
-                    @enderror
-                </label>
+                    <label class="w-full">
+                        <span class=" pb-2 ">mark :</span>
+                        <input class="w-full border border-gray-600 rounded-lg p-2 bg-gray-800 text-white focus:ring-2 focus:border-none focus:ring-[#fee814] focus:outline-none" type="text" wire:model="mark">
+                        @error('mark')
+                            <em class="text-red-500">{{ $message }}</em>
+                        @enderror
+                    </label>
 
-                <label class="flex flex-col gap-y-2">
-                    <span>equipment_type :</span>
-                    <select wire:model="equipment_type">
-                        <option selected hidden>choose equipment type</option>
-                        <option value="camera">camera</option>
-                        <option value="son">son</option>
-                        <option value="lumiere">lumiere</option>
-                        <option value="data/stockage">data/stockage</option>
-                        <option value="podcast">podcast</option>
-                        <option value="other">other</option>
-                    </select>
-                    @error('equipment_type')
-                        <em class="text-red-500">{{ $message }}</em>
-                    @enderror
-                </label>
-                    <input type="file" wire:model="image">
-                    @error('image')
-                        <em class="text-red-500">{{ $message }}</em>
-                    @enderror
-                    @if ($image)
-                        <img width="250" src="{{ $image->temporaryUrl() }}">
-                    @endif
-                <div>
-                    <button wire:click="cancel" type="button" x-on:click="$dispatch('close')">
-                        {{ __('Cancel') }}
-                    </button>
-                    <button type="submit" class="text-white bg-blue-500 mt-2 p-3 rounded-lg">Create</button>
+                    <label class="w-full">
+                        <span class=" pb-2 ">equipment_type :</span>
+                        <select class="w-full border border-gray-600 rounded-lg p-2 bg-gray-800 text-white focus:ring-2 focus:border-none focus:ring-[#fee814] focus:outline-none" wire:model="equipment_type">
+                            <option selected hidden>choose equipment type</option>
+                            <option value="camera">camera</option>
+                            <option value="son">son</option>
+                            <option value="lumiere">lumiere</option>
+                            <option value="data/stockage">data/stockage</option>
+                            <option value="podcast">podcast</option>
+                            <option value="other">other</option>
+                        </select>
+                        @error('equipment_type')
+                            <em class="text-red-500">{{ $message }}</em>
+                        @enderror
+                    </label>
+
+                    <label class="w-full">
+                        <span class=" pb-2 ">image :</span>
+                        <input class="w-full border border-gray-600 rounded-lg p-2 bg-gray-800 text-white focus:ring-2 focus:border-none focus:ring-[#fee814] focus:outline-none" type="file" wire:model="image">
+                        @error('image')
+                            <em class="text-red-500">{{ $message }}</em>
+                        @enderror
+                        @if ($image)
+                            <img width="250" src="{{ $image->temporaryUrl() }}">
+                        @endif
+                    </label>
+                    <div class="flex items-center justify-end gap-x-3 ">
+                        <button class="px-4 py-2 bg-gray-600 text-white rounded-lg  transition-colors duration-300" wire:click="cancel" type="button" x-on:click="$dispatch('close')">
+                            {{ __('Cancel') }}
+                        </button>
+                        <button type="submit" class="text-white bg-yellow-500  px-4 py-2 rounded-lg">Create</button>
+                    </div>
+                </form>
+            </div>
+        </x-modal>
+    </div>
+
+    <div class="flex items-center gap-x-16">
+        <input class="w-1/2 bg-[#1f2937] focus:ring-2 focus:border-none focus:ring-[#fee814] focus:outline-none rounded-md" type="text" wire:model.live="search" placeholder="search">
+        <select class="w-1/4 bg-[#1f2937] focus:ring-2 focus:border-none focus:ring-[#fee814] focus:outline-none rounded-md" wire:model.live="equipmentType">
+            <option disabled>choose equipment type</option>
+            <option value="all">all</option>
+            <option value="camera">camera</option>
+            <option value="son">son</option>
+            <option value="lumiere">lumiere</option>
+            <option value="data/stockage">data/stockage</option>
+            <option value="podcast">podcast</option>
+            <option value="other">other</option>
+        </select>
+        <button class="px-4 py-2 w-1/6 bg-yellow-500 text-white rounded-md  transition" wire:click="resetSearch">Reset Filter</button>
+    </div>
+
+    <div class="flex flex-wrap items-center justify-center gap-5">
+        @foreach ($equipments as $equipment)
+            <div class="flex flex-col w-[30vw] min-h-[40vh] bg-[#1f2937] rounded-md">
+                <div class="">
+                    @foreach ($equipment->images as $image)
+                        <img class="rounded-t-md" src="{{ asset('storage/' . $image->path) }}" alt="" >
+                    @endforeach
                 </div>
-            </form>
-        </div>
-    </x-modal>
-
-    <input type="text" wire:model.live="search" placeholder="search">
-    <select wire:model.live="equipmentType">
-        <option disabled>choose equipment type</option>
-        <option value="camera">camera</option>
-        <option value="son">son</option>
-        <option value="lumiere">lumiere</option>
-        <option value="data/stockage">data/stockage</option>
-        <option value="podcast">podcast</option>
-        <option value="other">other</option>
-    </select>
-    <button wire:click="resetSearch">reset</button>
-
-    <table>
-        <thead>
-            <tr>
-                <th>image</th>
-                <th>Reference</th>
-                <th>Mark</th>
-                <th>State</th>
-                <th>Equipment Type</th>
-                <th>Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($equipments as $equipment)
-                <tr wire:key="{{ $equipment->id }}">
-                    <td>
-                        @foreach ($equipment->images as $image)
-                            <img width="100" src="{{ asset('storage/' . $image->path) }}" alt="" >
-                        @endforeach
-                        {{-- @if ($equipment->images->isNotEmpty())
-                            <img width="100" src="{{ asset('storage/' . $equipment->images->first()->path ) }}" alt="">
-                        @endif --}}
-                    </td>
-                    <td>{{ $equipment->reference }}</td>
-                    <td>{{ $equipment->mark }}</td>
-                    <td>{{ $equipment->state }}</td>
-                    <td>{{ $equipment->equipment_type }}</td>
-                    <td>
-                        <button class="px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600 transition"
-                            x-on:click.prevent="$dispatch('open-modal', 'delete-equipment{{ $equipment->id }}')">{{ __('delete') }}</button>
+                <div class="flex items-center justify-between p-4">
+                    <div class="flex flex-col">
+                        <h1 class="">Reference : <span class="text-yellow-500">{{ $equipment->reference }}</span></h1>
+                        <h1 class="">Mark : <span class="text-yellow-500">{{ $equipment->mark }}</span></h1>
+                        <h1 class="">Equipment Type : <span class="text-yellow-500">{{ $equipment->equipment_type }}</span></h1>
+                        <h1 class="">State : <span class="text-yellow-500">{{ $equipment->state }}</span></h1>
+                    </div>
+                    <div class="flex flex-col items-center gap-y-3">
+                        <button class=""
+                            x-on:click.prevent="$dispatch('open-modal', 'delete-equipment{{ $equipment->id }}')">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#ef4444" d="M19 4h-3.5l-1-1h-5l-1 1H5v2h14M6 19a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V7H6z"/></svg>
+                        </button>
                         <x-modal name="delete-equipment{{ $equipment->id }}" :show="$errors->isNotEmpty()">
-                            <div class="p-5 gap-2 flex flex-col items-center">
+                            <div class="p-5 gap-2 flex flex-col items-center bg-[#111827]">
                                 <h1>Are You Sure You Want To Delete This Equipment</h1>
                                 <div>
-                                    <x-secondary-button x-on:click="$dispatch('close')">
+                                    <button x-on:click="$dispatch('close')" class="px-4 py-2 bg-gray-600 text-white rounded-lg  transition-colors duration-300">
                                         {{ __('Cancel') }}
-                                    </x-secondary-button>
+                                    </button>
                                     <button type="button"
                                         class="px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600 transition"
                                         wire:click='delete({{ $equipment->id }})'>
@@ -110,14 +112,15 @@
                         </x-modal>
 
                         <button wire:click='edit({{ $equipment->id }})'
-                            class="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition"
-                            x-on:click.prevent="$dispatch('open-modal', 'edit{{ $equipment->id }}' )">{{ __('edit') }}</button>
+                            class="px-4 py-2 "
+                            x-on:click.prevent="$dispatch('open-modal', 'edit{{ $equipment->id }}' )"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g fill="none" stroke="#3b82f6" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="m16.475 5.408l2.117 2.117m-.756-3.982L12.109 9.27a2.1 2.1 0 0 0-.58 1.082L11 13l2.648-.53c.41-.082.786-.283 1.082-.579l5.727-5.727a1.853 1.853 0 1 0-2.621-2.621"/><path d="M19 15v3a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V7a2 2 0 0 1 2-2h3"/></g></svg></button>
                         <x-modal name="edit{{ $equipment->id }}" :show="$errors->isNotEmpty()">
-                            <div class="p-5 gap-2 flex flex-col items-center">
-                                <form wire:submit='update({{ $equipment->id }})' class="flex flex-col items-center">
+                            <div class="p-5 gap-2 flex flex-col items-center bg-[#111827]">
+                                <form wire:submit='update({{ $equipment->id }})' class="flex flex-col w-full gap-y-3">
+                                    <h1 class="text-yellow-500 text-xl font-bold border-b pb-2">Edit Equipment</h1>
                                     <label class="flex flex-col gap-y-2">
                                         <span>reference :</span>
-                                        <input type="text" wire:model="reference">
+                                        <input type="text" wire:model="reference" class="w-full border border-gray-600 rounded-lg p-2 bg-gray-800 text-white focus:ring-2 focus:border-none focus:ring-[#fee814] focus:outline-none">
                                         @error('reference')
                                             <em class="text-red-500">{{ $message }}</em>
                                         @enderror
@@ -125,7 +128,7 @@
 
                                     <label class="flex flex-col gap-y-2">
                                         <span>mark :</span>
-                                        <input type="text" wire:model="mark">
+                                        <input type="text" wire:model="mark" class="w-full border border-gray-600 rounded-lg p-2 bg-gray-800 text-white focus:ring-2 focus:border-none focus:ring-[#fee814] focus:outline-none">
                                         @error('mark')
                                             <em class="text-red-500">{{ $message }}</em>
                                         @enderror
@@ -133,7 +136,7 @@
 
                                     <label class="flex flex-col gap-y-2">
                                         <span>equipment_type :</span>
-                                        <select wire:model="equipment_type">
+                                        <select wire:model="equipment_type" class="w-full border border-gray-600 rounded-lg p-2 bg-gray-800 text-white focus:ring-2 focus:border-none focus:ring-[#fee814] focus:outline-none">
                                             <option disabled>choose equipment type</option>
                                             <option value="camera">camera</option>
                                             <option value="son">son</option>
@@ -146,26 +149,30 @@
                                             <em class="text-red-500">{{ $message }}</em>
                                         @enderror
                                     </label>
-                                    <input type="file" wire:model="image">
-                                    @error('image')
-                                        <em class="text-red-500">{{ $message }}</em>
-                                    @enderror
-                                    <div>
-                                        <button wire:click="cancel" type="button" x-on:click="$dispatch('close')">
+                                    <label class="w-full">
+                                        <input type="file" wire:model="image" class="w-full border border-gray-600 rounded-lg p-2 bg-gray-800 text-white focus:ring-2 focus:border-none focus:ring-[#fee814] focus:outline-none">
+                                        @error('image')
+                                            <em class="text-red-500">{{ $message }}</em>
+                                        @enderror
+                                    </label>
+                                    <div class="flex items-center justify-end gap-x-3">
+                                        <button wire:click="cancel" type="button" x-on:click="$dispatch('close')" class="px-4 py-2 bg-gray-600 text-white rounded-lg  transition-colors duration-300">
                                             {{ __('Cancel') }}
                                         </button>
                                         <button type="submit"
-                                            class="text-white bg-blue-500 mt-2 p-3 rounded-lg">Update</button>
+                                            class="text-white bg-yellow-500  px-4 py-2 rounded-lg">Update</button>
                                     </div>
                                 </form>
 
                             </div>
                         </x-modal>
-                    </td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
+
+
 
 </div>
 
