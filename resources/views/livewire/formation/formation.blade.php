cc516b23964c7e9cb824f9835c806e9fac019a05
Abdellah-Majdoul <abdellahmajdoul57@gmail.com>
2025-02-27 15:51:04 +0000

    fix form formation

--- resources/views/livewire/formation/formation.blade.php
+++ resources/views/livewire/formation/formation.blade.php
@@ -37,13 +37,13 @@
             </div>
 
             <div class="absolute flex top-4 right-4 space-x-3 cursor-pointer">
-                <svg wire:click="delete({{ $formation->id }})" xmlns="http://www.w3.org/2000/svg" fill="none"
+                <svg wire:click="delete('{{ $formation->id }}')"xmlns="http://www.w3.org/2000/svg" fill="none"
                     viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
                     class="h-6 w-6 text-gray-400 hover:text-red-600 transition-colors">
                     <path stroke-linecap="round" stroke-linejoin="round"
                         d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                 </svg>
-                <svg wire:click="edit({{ $formation->id }})" xmlns="http://www.w3.org/2000/svg" fill="none"
+                <svg wire:click="edit('{{ $formation->id }}')" xmlns="http://www.w3.org/2000/svg" fill="none"
                     viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
                     class="h-6 w-6 text-gray-400 hover:text-blue-600 transition-colors">
                     <path stroke-linecap="round" stroke-linejoin="round"
@@ -92,7 +92,7 @@
                     </div>
                 </div>
                 <div class="flex justify-between mt-6">
-                    <button type="button" wire:click="cancel" class="px-4 py-2 bg-gray-700 text-white rounded-lg">Cancel</button>
+                    <button  wire:click="cancel" class="px-4 py-2 bg-gray-700 text-white rounded-lg">Cancel</button>
                     <button type="submit" class="px-4 py-2 bg-yellow-500 text-white rounded-lg">{{ $updateData ? 'Update' : 'Create' }}</button>
                 </div>
             </form>
